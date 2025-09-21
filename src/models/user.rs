20f4419960ac9e11557c175e7a9cbd0542ca22cb
Antony Gakuru <antony123muriithi@gmail.com>
2025-09-21 23:32:30 +0300

    add user models

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,5 +1,19 @@
+use diesel::prelude::*;
+use crate::schema::users;
+use serde::{Debug, Serialize, Deserialize};
 
-
+#[derive(Debug, Deserialize, Serialize, Queryable)]
 pub struct User{
-    
+    id: i32,
+    first_name: String,
+    last_name: String,
+    username: String,
+}
+
+#[derive(Debug, Deserialize, Serialize, Insertable)]
+#[diesel(table_name = "users")]
+pub struct NewUser{
+    first_name: String,
+    last_name: String,
+    username: String,
 }
