3918e9fe16794080014bfce3ad64266b778cd12a
Antony Gakuru <antony123muriithi@gmail.com>
2025-09-22 01:52:09 +0300

    add posts migration

--- migrations/2025-09-21-224535-0000_create_posts/up.sql
+++ migrations/2025-09-21-224535-0000_create_posts/up.sql
@@ -1,1 +1,10 @@
 -- Your SQL goes here
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE posts(
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    caption VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+)
