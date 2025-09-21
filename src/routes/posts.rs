4396ca20efc8a532806c9bf56b916b3dfc631c86
Antony Gakuru <antony123muriithi@gmail.com>
2025-09-22 02:23:46 +0300

    fx post routes

--- src/routes/posts.rs
+++ src/routes/posts.rs
@@ -2,6 +2,7 @@ use actix_web::web;
 use crate::handlers;
 
 pub fn config(cfg: &mut web::ServiceConfig){
-    cfg.route("/posts", web::get().to(handlers::get_posts));
-    cfg.route("/posts", web::post().to(handlers::make_post));
+    cfg
+        .route("/posts", web::get().to(handlers::get_posts))
+        .route("/posts", web::post().to(handlers::make_post));
 }
